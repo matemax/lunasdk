1e971f1c7ffca64c9abd7162b69e95770c52113f
Maxim Limonov <m.limonov@visionlabs.ru>
2019-04-19 00:10:53 +0300

    update setup.py

---
+++ lunavl/version.py
@@ -0,0 +1,7 @@
+"""
+Module contain version of  package
+
+Attributes:
+    VERSION
+"""
+VERSION: str = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+from lunavl.version import VERSION
+
+setup(
+    name="lunavl",
+    description="Python interface for VisionLabs Luna platform",
+    version=VERSION,
+    author="VisionLabs",
+    author_email="m.limonov@visionlabs.ru",
+    packages=find_packages(exclude=["docs", "tests", "tests.*"]),
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'numpy',
+    ]
+)
