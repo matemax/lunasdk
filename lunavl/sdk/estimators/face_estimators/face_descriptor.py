bbd90bb45a2786a710879a110236f81c060815e0
Maxim Limonov <m.limonov@visionlabs.ru>
2019-07-17 14:17:52 +0300

    realize FaceDescriptorBatch

--- lunavl/sdk/estimators/face_estimators/face_descriptor.py
+++ lunavl/sdk/estimators/face_estimators/face_descriptor.py
@@ -6,42 +6,69 @@ from typing import Union, Optional, List
 from lunavl.sdk.estimators.face_estimators.warper import Warp, WarpedImage
 
 
-class FaceDescriptorBatch(BaseEstimation):
-
-    #  pylint: disable=W0235
-    def __init__(self, coreEstimation: IDescriptorBatchPtr):
-        super().__init__(coreEstimation)
-
-    def asDict(self) -> Union[dict, list]:
-        return {"descriptor": self.coreEstimation.getData()}
-
-
 class FaceDescriptor(BaseEstimation):
     """
     Descriptor
+
+    Attributes:
+        garbageScore (float): garbage score
     """
 
     #  pylint: disable=W0235
-    def __init__(self, coreEstimation: IDescriptorPtr):
+    def __init__(self, coreEstimation: IDescriptorPtr, garbageScore: float = 0.0):
         super().__init__(coreEstimation)
+        self.garbageScore = garbageScore
 
     def asDict(self) -> Union[dict, list]:
         return {"descriptor": self.coreEstimation.getData()}
 
     def asDict(self) -> Union[dict, list]:
-        return {"descriptor": self.coreEstimation.getData()}
+        return {"descriptor": self.coreEstimation.getData(),
+                "score": self.garbageScore}
 
     @property
     def rawDescriptor(self):
-        return self.coreEstimation.getData()
+        err, bytes = self.coreEstimation.save()
+        if err.isError():
+            raise ValueError(234567)
+        return bytes
 
     @property
     def prettyDescriptor(self):
         return self.coreEstimation.getDescriptor()
 
     @property
+    def asBytes(self):
+        return self.coreEstimation.getData()
+
+    @property
     def model(self):
         return self.coreEstimation.getModelVersion()
+
+
+class FaceDescriptorBatch(BaseEstimation):
+
+    #  pylint: disable=W0235
+    def __init__(self, coreEstimation: IDescriptorBatchPtr, scores: Optional[List[float]] = None):
+        super().__init__(coreEstimation)
+        if scores is None:
+            self.scores = [0.0 for _ in range(coreEstimation.getMaxCount())]
+        else:
+            self.scores = scores
+
+    def __len__(self):
+        return self._coreEstimation.getMaxCount()
+
+    def asDict(self) -> Union[dict, list]:
+        return {"descriptors": [descriptor.asDict() for descriptor in self]}
+
+    def __getitem__(self, i):
+        return FaceDescriptor(self._coreEstimation.getDescriptorFast(i), self.scores[i])
+
+    def __iter__(self) -> FaceDescriptor:
+        itemCount = self._coreEstimation.getMaxCount()
+        for index in range(itemCount):
+            yield FaceDescriptor(self._coreEstimation.getDescriptorFast(index), self.scores[index])
 
 
 class FaceDescriptorEstimator(BaseEstimator):
@@ -76,7 +103,7 @@ class FaceDescriptorEstimator(BaseEstimator):
         res = self._coreEstimator.extractFromWarpedImage(warp.warpedImage.coreImage, descriptor)
         if res.isError:
             raise ValueError("12343")
-        return FaceDescriptor(descriptor)
+        return FaceDescriptor(descriptor, res.value)
 
     def estimateWarpsBatch(self, warps: List[Union[Warp, WarpedImage]], aggregate: bool = False,
                            descriptorBatch: Optional[FaceDescriptorBatch] = None):
@@ -87,10 +114,13 @@ class FaceDescriptorEstimator(BaseEstimator):
         else:
             descriptorBatch = self.descriptorDescriptorBatchFactory(len(warps))
         if aggregate:
-            self._coreEstimator.extractFromWarpedImageBatch([warp.warpedImage.coreImage for warp in warps],
-                                                            descriptorBatch, aggregate, len(warps))
+            res, scores = self._coreEstimator.extractFromWarpedImageBatch(
+                [warp.warpedImage.coreImage for warp in warps],
+                descriptorBatch, aggregate, len(warps))
         else:
-            self._coreEstimator.extractFromWarpedImageBatch([warp.warpedImage.coreImage for warp in warps],
-                                                            descriptorBatch, len(warps))
-        return descriptorBatch
-
+            res, scores = self._coreEstimator.extractFromWarpedImageBatch(
+                [warp.warpedImage.coreImage for warp in warps],
+                descriptorBatch, len(warps))
+        if res.isError:
+            raise ValueError("12343")
+        return FaceDescriptorBatch(descriptorBatch, scores)
