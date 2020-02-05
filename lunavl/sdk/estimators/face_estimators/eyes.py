32e6fb4aeebc0c19666e101db70b28bcfa95ea0d
n.feofanov <n.feofanov@visionlabs.ru>
2020-02-05 10:01:27 +0300

    rm landmark68 from estimation typing

--- lunavl/sdk/estimators/face_estimators/eyes.py
+++ lunavl/sdk/estimators/face_estimators/eyes.py
@@ -300,7 +300,7 @@ class GazeEstimator(BaseEstimator):
     #  pylint: disable=W0221
     @CoreExceptionWrap(LunaVLError.EstimationEyesGazeError)
     def estimate(
-        self, transformedLandmarks: Union[Landmarks5, Landmarks68], warp: Union[Warp, WarpedImage]
+        self, transformedLandmarks: Landmarks5, warp: Union[Warp, WarpedImage]
     ) -> GazeDirection:
         """
         Estimate a gaze direction
