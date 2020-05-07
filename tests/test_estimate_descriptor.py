43d69e580dcf036fc0fb21a4d99259a97dfe93bf
matemax <matemaks@ya.ru>
2020-05-07 16:22:59 +0300

    Update tests/test_estimate_descriptor.py
    Co-authored-by: Zeskbest <tim-ef@ya.ru>

--- tests/test_estimate_descriptor.py
+++ tests/test_estimate_descriptor.py
@@ -23,7 +23,6 @@ from lunavl.sdk.globals import DEFAULT_HUMAN_DESCRIPTOR_VERSION as DHDV
 from tests.base import BaseTestClass
 from tests.resources import WARP_WHITE_MAN, HUMAN_WARP
 
-# from unittest.case import _SubTest
 
 EFDVa = EXISTENT_FACE_DESCRIPTOR_VERSION_ABUNDANCE = [46, 52, 54, 56]
 
