8432054e05e2815ffbbead4ab426550a65b4aadb
Nikita <51211976+DeusAnimo@users.noreply.github.com>
2020-08-14 02:46:05 +0300

    Luna 4169 update tests with mask (#89)
    * update mask tests
    
    * replace mask
    
    * update func description\renaming
    
    * fix code stile
    
    * fix code stile
    
    * added test data for masks
    
    * pep8
    
    * new warp image\fix score
    
    * some fix
    
    * refactor
    
    * refactor
    
    Co-authored-by: n.khaibulin <n.khaibulin@visionlabs.ru>

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,12 +1,14 @@
-from lunavl.sdk.estimators.face_estimators.mask import Mask, MaskEstimator
-from lunavl.sdk.faceengine.setting_provider import DetectorType
-from lunavl.sdk.image_utils.image import VLImage
+from collections import namedtuple
+from typing import Dict
 
+from lunavl.sdk.estimators.face_estimators.mask import Mask, MaskEstimator
+from lunavl.sdk.image_utils.image import VLImage
+from sdk.estimators.face_estimators.facewarper import FaceWarpedImage
 from tests.base import BaseTestClass
-from tests.resources import CLEAN_ONE_FACE, FACE_WITH_MASK, OCCLUDED_FACE, MASK_NOT_IN_PLACE
+from tests.resources import WARP_CLEAN_FACE, FACE_WITH_MASK, OCCLUDED_FACE, MASK_NOT_IN_PLACE
 from tests.schemas import jsonValidator, MASK_SCHEMA
 
-MASK_PROPERTIES = [key for key in Mask.__dict__.keys() if not (key.startswith("_") or key == "asDict")]
+MaskProperties = namedtuple("MaskProperties", ("maskInPlace", "maskNotInPlace", "noMask", "occludedFace"))
 
 
 class TestMask(BaseTestClass):
@@ -22,55 +24,32 @@ class TestMask(BaseTestClass):
         super().setup_class()
         cls.maskEstimator = cls.faceEngine.createMaskEstimator()
 
-        defaultDetector = cls.faceEngine.createFaceDetector(DetectorType.FACE_DET_DEFAULT)
-        warper = cls.faceEngine.createFaceWarper()
-        cls.warpImageWithMask = warper.warp(defaultDetector.detectOne(VLImage.load(filename=FACE_WITH_MASK)))
-        cls.warpImageNoMask = warper.warp(defaultDetector.detectOne(VLImage.load(filename=CLEAN_ONE_FACE)))
-        cls.warpImageMaskNotInPlace = warper.warp(defaultDetector.detectOne(VLImage.load(filename=MASK_NOT_IN_PLACE)))
-        cls.warpImageOccludedFace = warper.warp(defaultDetector.detectOne(VLImage.load(filename=OCCLUDED_FACE)))
+        cls.warpImageWithMask = FaceWarpedImage(VLImage.load(filename=FACE_WITH_MASK))
+        cls.warpImageNoMask = FaceWarpedImage(VLImage.load(filename=WARP_CLEAN_FACE))
+        cls.warpImageMaskNotInPlace = FaceWarpedImage(VLImage.load(filename=MASK_NOT_IN_PLACE))
+        cls.warpImageOccludedFace = FaceWarpedImage(VLImage.load(filename=OCCLUDED_FACE))
 
-    @staticmethod
-    def assertMaskEstimation(mask: Mask):
+    def assertMaskEstimation(self, mask: Mask, expectedEstimationResults: Dict[str, float]):
         """
-        Function checks if an instance is Mask class
+        Function checks if the instance belongs to the Mask class and compares the result with what is expected.
 
         Args:
             mask: mask estimation object
+            expectedEstimationResults: dictionary with probability scores
         """
         assert isinstance(mask, Mask), f"{mask.__class__} is not {Mask}"
-        for propertyName in MASK_PROPERTIES:
-            property = getattr(mask, propertyName)
-            assert isinstance(property, float), f"{propertyName} is not float"
-            assert 0 <= property < 1, f"{propertyName} is out of range [0,1]"
 
-    @staticmethod
-    def assertMaskPropertyReply(replyMask: Mask, expectedPredominantProperty: str, lowerThreshold: float = 0.9):
-        """
-        Function checks predominant property from reply
-
-        Args:
-            replyMask: mask estimation object
-            expectedPredominantProperty: expected property of the mask object
-            lowerThreshold: lower threshold of estimation
-        """
-        lowerProbabilitySet = set(MASK_PROPERTIES) - {expectedPredominantProperty}
-        actualPropertyResult = getattr(replyMask, expectedPredominantProperty)
-        assert (
-            actualPropertyResult > lowerThreshold
-        ), f"Value of the Mask estimation '{actualPropertyResult}' is less than '{lowerThreshold}'"
-        for lowerProp in lowerProbabilitySet:
-            assert actualPropertyResult > replyMask.__getattribute__(
-                lowerProp
-            ), f"Value of the Mask estimation '{expectedPredominantProperty}' is less than '{lowerProp}'"
-
-    def test_estimate_mask(self):
-        """
-        Test mask estimations
-        """
-        for warp in (self.warpImageWithMask, self.warpImageWithMask.warpedImage):
-            with self.subTest(warp=type(warp).__name__):
-                mask = TestMask.maskEstimator.estimate(warp)
-                self.assertMaskEstimation(mask)
+        for propertyName in expectedEstimationResults:
+            with self.subTest(propertyName=propertyName):
+                actualPropertyResult = getattr(mask, propertyName)
+                assert isinstance(actualPropertyResult, float), f"{propertyName} is not float"
+                assert 0 <= actualPropertyResult < 1, f"{propertyName} is out of range [0,1]"
+                self.assertAlmostEqual(
+                    actualPropertyResult,
+                    expectedEstimationResults[propertyName],
+                    delta=0.001,
+                    msg=f"property value '{propertyName}' is incorrect",
+                )
 
     def test_estimate_mask_as_dict(self):
         """
@@ -85,31 +64,30 @@ class TestMask(BaseTestClass):
         """
         Test mask estimations with mask exists on the face
         """
+        expectedResult = MaskProperties(0.977, 0.022, 0.001, 0.001)
         mask = TestMask.maskEstimator.estimate(self.warpImageWithMask)
-        self.assertMaskEstimation(mask)
-        self.assertMaskPropertyReply(mask, "maskInPlace")
+        self.assertMaskEstimation(mask, expectedResult._asdict())
 
     def test_estimate_without_mask_on_the_face(self):
         """
         Test mask estimations without mask on the face
         """
+        expectedResult = MaskProperties(0.007, 0.071, 0.897, 0.025)
         mask = TestMask.maskEstimator.estimate(self.warpImageNoMask)
-        self.assertMaskEstimation(mask)
-        self.assertMaskPropertyReply(mask, "noMask")
+        self.assertMaskEstimation(mask, expectedResult._asdict())
 
     def test_estimate_mask_not_in_place(self):
         """
         Test mask estimations with mask exists on the face and is not worn properly
         """
+        expectedResult = MaskProperties(0.042, 0.386, 0.003, 0.567)
         mask = TestMask.maskEstimator.estimate(self.warpImageMaskNotInPlace)
-        self.assertMaskEstimation(mask)
-        self.assertMaskPropertyReply(mask, "maskNotInPlace", 0.6)
+        self.assertMaskEstimation(mask, expectedResult._asdict())
 
     def test_estimate_mask_occluded_face(self):
         """
         Test mask estimations with face is occluded by other object
         """
+        expectedResult = MaskProperties(0.001, 0.141, 0.326, 0.531)
         mask = TestMask.maskEstimator.estimate(self.warpImageOccludedFace)
-        self.assertMaskEstimation(mask)
-        # for the method with "occluded face", the values may differ (the test is unstable)
-        self.assertMaskPropertyReply(mask, "occludedFace", 0.4)
+        self.assertMaskEstimation(mask, expectedResult._asdict())
