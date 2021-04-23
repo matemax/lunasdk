c2ac99e0c5e8a3ab04f242b8de0859d1e0c4c8ae
AlexeyPichugin <a.o.pichugin@outlook.com>
2021-04-23 13:57:18 +0300

    Fixed crop estimation bug

--- examples/sdk_examples/mask_estimation.py
+++ examples/sdk_examples/mask_estimation.py
@@ -20,7 +20,7 @@ def estimateMedicalMask():
 
     medicalMaskEstimator = faceEngine.createMaskEstimator()
     # Estimate from detection
-    pprint.pprint(medicalMaskEstimator.estimate(faceDetection).asDict())
+    pprint.pprint(medicalMaskEstimator.estimate(faceDetection.coreEstimation).asDict())
 
     # Estimate from wrap
     warper = faceEngine.createFaceWarper()
