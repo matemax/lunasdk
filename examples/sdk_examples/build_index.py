97497868088d26e449169aba6e32ccc8ba46ad8e
matemax <matemaks@ya.ru>
2022-08-30 11:52:24 +0300

    async search

--- examples/sdk_examples/build_index.py
+++ examples/sdk_examples/build_index.py
@@ -2,13 +2,13 @@
     * build index with descriptors
     * search for descriptors with the shorter distance to passed descriptor
 """
+import asyncio
 import pprint
-
-from resources import EXAMPLE_1, EXAMPLE_O
 
 from lunavl.sdk.faceengine.engine import VLFaceEngine
 from lunavl.sdk.faceengine.setting_provider import DetectorType
 from lunavl.sdk.image_utils.image import VLImage
+from resources import EXAMPLE_1, EXAMPLE_O
 
 
 def buildDescriptorIndex():
@@ -37,5 +37,34 @@ def buildDescriptorIndex():
     pprint.pprint(f"result: {result}")
 
 
+async def asyncBuildDescriptorIndex():
+    """
+    Async search by index.
+    """
+    """
+    Build index and search.
+    """
+    faceEngine = VLFaceEngine()
+    detector = faceEngine.createFaceDetector(DetectorType.FACE_DET_V3)
+    warper = faceEngine.createFaceWarper()
+    extractor = faceEngine.createFaceDescriptorEstimator()
+    descriptorsBatch = faceEngine.createFaceDescriptorFactory().generateDescriptorsBatch(2)
+
+    for image in (EXAMPLE_O, EXAMPLE_1):
+        vlImage = VLImage.load(filename=image)
+        faceDetection = await detector.detectOne(vlImage, asyncEstimate=True)
+        warp = warper.warp(faceDetection)
+        faceDescriptor = await extractor.estimate(warp.warpedImage, asyncEstimate=True)
+        descriptorsBatch.append(faceDescriptor)
+
+    indexBuilder = faceEngine.createIndexBuilder()
+    indexBuilder.appendBatch(descriptorsBatch)
+    index = indexBuilder.buildIndex()
+    pprint.pprint(index[0])
+    result = await index.search(faceDescriptor, 2, asyncSearch=True)
+    pprint.pprint(f"result: {result}")
+
+
 if __name__ == "__main__":
     buildDescriptorIndex()
+    asyncio.run(asyncBuildDescriptorIndex())
